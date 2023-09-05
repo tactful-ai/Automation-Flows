7e57b157edd5ab12e38f4b847761282ec09c7ccb
Mohamed Abobakr <mabobakr24@gmail.com>
2023-09-05 03:44:28 +0300

    Add files via upload

---
+++ Automation-Flows-Service/src/flows/acc_mngmt/subFlows/subscribe.ts
@@ -0,0 +1,38 @@
+
+
+
+import flow ,{ Triggers,IExecuteParam,  FacebookFlow, Card, WebchatFlow, FlowButton}from 'automation-sdk';
+
+
+export function Subscribe() {
+    
+    const Flow = new flow.WebchatFlow("subscribe_flow", "intern_category", "1.0");
+    Flow
+        
+
+       
+    .api("https://localhost:4000/subscribe","POST",{},{
+        userId:"{{params.userId}}",
+        
+    })
+    .if(($: IExecuteParam) => {
+        const bool = $.context.api.response.json.bool
+        return bool ;
+      })
+          .text([['Loading',3]])
+          .text([["{{api.response.json.msg}}",1]])
+        .jump("choice_category.choice_flow.webchat@1.0")  
+
+      .else()
+          .text([['{{api.response.json.msg}}',1]])
+         .jump("choice_category.choice_flow.webchat@1.0") 
+      .endIf();
+            
+    
+    
+     
+
+    return Flow
+}
+
+
